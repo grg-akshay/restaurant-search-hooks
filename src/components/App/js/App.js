31eb387d0db2cb48cc84dc51ecbefa2b322b4a96
root <root@LAPTOP-LT01E602.localdomain>
2019-08-24 21:37:43 +0530

    Initial setup

---
+++ src/components/App/js/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../styles/App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      React with hooks boilerplate!
+    </div>
+  );
+}
+
+export default App;
